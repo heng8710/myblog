081c8ac6fff284ac35f6e34238e7cb56d1e34aa8
heng8710 <821600682@qq.com>
2016-01-19 14:50:41 +0800

    Update MyApp.java
    去掉application在web.xml中声明的方式，改为使用jersey的注解方式。

--- myblog-web/src/main/java/org/heng/myblog/app/MyApp.java
+++ myblog-web/src/main/java/org/heng/myblog/app/MyApp.java
@@ -3,15 +3,24 @@ package org.heng.myblog.app;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.heng.myblog.controller.CategoryController;
 import org.springframework.web.filter.RequestContextFilter;
+import javax.ws.rs.ApplicationPath;
 
+@ApplicationPath("blog")
 public class MyApp extends ResourceConfig {
 	/**
      * Register JAX-RS application components.
      */
     public MyApp() {
-        register(RequestContextFilter.class);
+    	
+    	//packages("resource");
         
-        register(CategoryController.class);
+        //referer filter
+        
+        //cache headers
+    	
+        //register(RequestContextFilter.class);
+        
+       // register(CategoryController.class);
 //        register(SpringSingletonResource.class);
 //        register(SpringRequestResource.class);
 //        register(CustomExceptionMapper.class);
